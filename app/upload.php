f273ad8482d5ca2105925d786581c4904b90e339
黄树鹏 <825844231@qq.com>
2018-12-01 15:00:01 +0800

    修改提交的

--- app/upload.php
+++ app/upload.php
@@ -3,30 +3,53 @@ require_once('response.php');
 require_once('base.php');
  
 $index = 0; 
-echo "获取上传的图片";
-echo $_FILES;
-$dir_base = "../img/";
+$success_index = 0;
+$remove_path = ""; 
+$img_paths; 
 foreach($_FILES as $file){
-    echo "开始上传图片";
-    $upload_file_name = 'upload_file' . $index;        //对应index.html FomData中的文件命名
+   
+    $upload_file_name = 'upload_file'.$index;        //对应index.html FomData中的文件命名
+ 
+    if ($_FILES[$upload_file_name]["error"] > 0)
+    {
+		echo "Return Code: " . $_FILES[$upload_file_name]["error"] . "<br />";
+    }
     $filename = $_FILES[$upload_file_name]['name']; 
-    echo $filename;
+   
     $gb_filename = iconv('utf-8','gb2312',md5(uniqid()).".jpg");    //名字转换成gb2312处理
-    echo "保存的文件:".$gb_filename;
-    echo "临时文件：".$_FILES[$upload_file_name]['tmp_name'];
-    
+   
    
     $rEFileTypes = "/^\.(jpg|jpeg|gif|png){1}$/i"; 
-    $isMoved = @move_uploaded_file ( $_FILES[$upload_file_name]['tmp_name'], $dir_base.$gb_filename);        //上传文件
-    echo "----";
-    echo  $dir_base.$gb_filename;
-    echo "------"; 
-    if ($isMove){
-        echo "上传成功";
+    $move_path =  '../img/'.$gb_filename; 
+  
+    
+    if (move_uploaded_file ( $_FILES[$upload_file_name]['tmp_name'], $move_path )){
+        if (!empty($img_paths)){
+            $img_paths = $img_paths.","; 
+            $remove_path = $remove_path.","; 
+        }
+        $img_paths = $img_paths.$domain_name."/img/".$gb_filename;
+        $remove_path = $remove_path."../img/".$gb_filename; 
+        $success_index++; 
     }else{
-        echo "上传失败";
+        
     }
     $index++;
 }
+
+if ($index == $success_index){
+    Response::json("0","上传成功",array(
+        "imgs"=>$img_paths
+    ));
+}else{
+    $imgArray = explode(",",$remove_path);
+     
+    foreach($imgArray as $value){
+        unlink($value); 
+    }
+    Response::failure("101","上传失败"); 
+}
+
+
 ?>
 
