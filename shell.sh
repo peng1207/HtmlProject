d1a4818cfef138adc7ea2ea73751e4615ebb72f1
chengzong liang <chengzongliang@chengzongdeMac-mini.local>
2018-12-07 18:37:07 +0800

    提交新的数据

--- shell.sh
+++ shell.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/expect
 echo "开始执行脚本"
+
 echo $PWD
 root_path="";
 root_path="$PWD"
@@ -6,8 +8,7 @@ echo $root_path
  
 service_name="root@huangshupeng.cn:/usr/share/nginx/html"
 echo "上传app目录 请输入密码"
-scp -r $root_path"/app/" $service_name
- 
+ scp -r $root_path"/app/" $service_name
 echo "上传page目录 请输入密码"
 scp -r $root_path"/page/" $service_name
 echo "上传js目录 请输入密码"
