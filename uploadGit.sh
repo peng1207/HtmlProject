0f728ebc4f6a5d1d93f7ab33b276597f05014032
chengzong liang <chengzongliang@chengzongdeMac-mini.local>
2018-12-07 17:16:38 +0800

    提交新的数据

--- uploadGit.sh
+++ uploadGit.sh
@@ -4,12 +4,12 @@ git pull origin master
 git status
 git add .
 git status
-if [$name != '']; then
-    git commit -m $name
-else
-    git commit -m "提交新的数据"
-fi
-
+#if [$name != '']; then
+#    git commit -m $name
+#else
+#    git commit -m "提交新的数据"
+#fi
+  git commit -m "提交新的数据"
 git status
 git push -u origin master
 echo "执行完成"
